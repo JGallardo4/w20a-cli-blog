c44f1b5026f402087ac3663901a4d2ff3afd78f6
Juan Gallardo <juan.gallardo.ab@gmail.com>
2021-02-11 20:16:30 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+# Now that we can connect our python application to our DB, let's go ahead and create a strange application! This application will allow users to create blog posts from their terminal and see posts as well!
+
+import db
+
+def writeNewPost(user):
+	content = input("Your post:\n")
+	db.createNewPost(user, content)
+
+def showAllPosts():
+	for row in db.getPosts():
+		print(row)
+
+def app():
+	username = input("Create a username: ")
+
+	print("Options:\n")
+	print("1. Write a new post")
+	print("2. See all other posts")
+	print("3. Quit")
+
+	while True:
+		try:
+			selection = int(input("\nYour choice: "))
+			if selection < 1 or selection > 3:
+				raise ValueError("Invalid option")
+			break
+		except:
+			print("Please make a valid selection")
+			continue
+
+	if (selection == 1):
+		writeNewPost(username)
+	elif (selection == 2):
+		showAllPosts()
+	elif (selection == 3):
+		quit()
+
+while True:
+	app()
+	
+# ADDITIONAL REQUIREMENTS (Feb 11):
+
+#     Handle empty result sets from the SELECT statement
+#     Display the row count from your SELECT statement
+#     Add try/except to catch possible mariadb exceptions
+
+ 
+
+# HINTS:
+
+#     I recommend you make extensive use of functions for this assignment. You should at least have 2 functions, one for running the insert and one for running the select
+
+ 
+
+# BONUS:
+
+#     Make the application run in a loop. After the user selects option 1 or option 2 and enters the information if needed, the application should keep running. This also requires you to give the user an option to exit the application
+#     Give the application a real login functionality. This requires you to add modifications to the database to create a users table. The user should be prompted to enter a username and password to gain access. These users can be created using Dbeaver for testing.
+
+ 
+
+# ADDITIONAL BONUS (Feb 11):
+
+#     Avoid duplicating your try/except code between multiple queries.  Is there a way to make this more efficient and less repetitive by leveraging functions (or even objects if you're feeling really keen)???
---
+++ db.py
@@ -0,0 +1,46 @@
+import dbcreds
+import mariadb
+import sys
+# set connection and cursor as global variables
+connection = None
+cursor = None
+def connect():
+    return mariadb.connect(
+        user=dbcreds.user,
+        password=dbcreds.password,
+        host=dbcreds.host,
+        port=dbcreds.port,
+        database=dbcreds.database
+    )
+def getPosts():
+    try:
+        connection = connect()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM blog_post")
+        result = cursor.fetchall()
+    except:
+        print("DB Error")
+        quit()    
+    else:        
+        if (cursor != None):
+            cursor.close()
+        if (connection != None):        
+            connection.close()
+        return result
+
+def createNewPost(user, content):
+    try:
+        connection = connect()
+        cursor = connection.cursor()
+        cursor.execute(
+            "INSERT INTO blog_post (username, content) VALUES (?, ?)",
+            [user, content])
+        connection.commit()
+    except:
+        print("DB Error")
+        quit()    
+    else:        
+        if (cursor != None):
+            cursor.close()
+        if (connection != None):        
+            connection.close()
